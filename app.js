40a1c421294abbb274337813a5ee53d550416032
Elshibani Omar <amr.omar80@gmail.com>
2018-04-18 14:50:09 -0600

    Add routes and models

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 
 const app = express();
+
+// Map Global promise - get rid of warning
+// mongoose.Promise = global.Promise;
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev')
+  .then(() => console.log('MongoDB Connected....'))
+  .catch((err) => console.log(err));
+
+// Load idea model
+require('./models/idea');
+const Idea = mongoose.model('ideas');
 
 // Handlebars middleware
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Index Route
 app.get('/', (req, res) => {
@@ -22,6 +39,54 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+// Idea index page
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({date: 'desc'})
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas
+      });
+    })
+  
+});
+
+// Add idea form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+// Process Form
+app.post('/ideas', (req, res) => {
+  let errors = [];
+  console.log(req.body)
+  if(!req.body.title) {
+    errors.push({text: 'Please add a title'});
+  }
+  if(!req.body.details) {
+    errors.push({text: 'Please add some details'});
+  }
+  
+  if(errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    }
+
+    new Idea(newUser)
+      .save()
+      .then(idea => {
+        res.redirect('/ideas');
+      })
+  }
 });
 
 const port = 5000;
