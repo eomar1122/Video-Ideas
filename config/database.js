ab8bacf7ec8ee326f3461be5e775cb15d57007c9
Elshibani Omar <amr.omar80@gmail.com>
2018-04-18 21:06:34 -0600

    Add mLab

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV === 'production') {
-  module.exports = {mongoURI: 'mongodb://root:123456@ds249299.mlab.com:49299/vidjot'}
-} else {
-  module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
-}
